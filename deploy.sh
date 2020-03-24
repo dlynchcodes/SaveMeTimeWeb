e9aeeb8c1a977d5d2d79161cfad779c294a07966
Daniel Lynch <danielwewo@gmail.com>
2020-03-23 20:15:15 -0700

    Removed files from repo
    .rsync-filter, deploy.sh, server.pem, and simple-https-server.py are not necessary and are only my preferred way of developing.

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Variables
-remoteUserName="dlynch"
-remoteServer="savemeti.me"
-remotePath="/srv/savemetime/Web/"
-
-rsync -arvz -e 'ssh -p 5656' --progress --delete --filter=':- .rsync-filter' ./ ${remoteUserName}@${remoteServer}:${remotePath}
